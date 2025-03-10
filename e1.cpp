efe2aecf9179bdcd15c83e5005451df3e31a417f
SOrtiz1967 <166780651+SOrtiz1967@users.noreply.github.com>
2025-03-10 12:46:03 -0300

    Add files via upload

---
+++ e1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+Crear la estructura Empleado que tiene un nombre, apellido, fecha de nacimiento,
+sexo y salario.Se pide la función empleadoConMayorSueldo que recibe por parámetro 
+un vector con todos los empleados de la empresa y otro vector vacío donde se
+guardarán los empleados que tienen salario menor o igual a $400.000.La función deberá
+recorrer todos los empleados de la empresa y retornar el de mayor sueldo. Además, a medida
+que va recorriendo los empleados, debe agregar al otro vector los empleados cuyo salario
+es menor o igual a $400.000.Por lo tanto, luego de llamar a la función,
+se quiere mostrar en el main el empleado con mayor sueldo y mostrar el nombre
+y apellido de todos los empleados con salario menor o igual a $400.000.
+*/
+struct empleado {
+    string nombre;
+    string apellido;
+    int f_nacimiento;
+    string sexo;
+    int salario;
+};
+void ingreso(vector <empleado> &nomina){
+    while(true){
+        string var;
+        empleado aux;
+        cout<<"queres hacer un ingreso"<<endl;
+        cin>>var;
+        if(var=="no")
+        {
+            break;
+        }
+        cout<<"ingresa el nombre del empleado"<<endl;
+        cin>>aux.nombre;
+        cout<<"ingresa el apellido"<<endl;
+        cin>>aux.apellido;
+        cout<<"ingresa el nacimiento del empleado"<<endl;
+        cin>>aux.f_nacimiento;
+        cout<<"ingresa el sexo del empleado"<<endl;
+        cin>>aux.sexo;
+        cout<<"ingresa el sueldo del empleado"<<endl;
+        cin>>aux.salario;
+
+        
+        nomina.push_back(aux);
+    }
+    };
+    empleado empleadoConMayorSueldo(vector <empleado> nomina, vector <empleado> &nomina_barata)
+    {
+        empleado sobre=nomina[0];
+        for (int i = 0; i < nomina.size(); i++)
+        {
+            if(sobre.salario<nomina[i].salario){
+                sobre=nomina[i];
+            }
+            if(nomina[i].salario<=400000){
+                nomina_barata.push_back(nomina[i]);
+            }
+        }
+        
+        return sobre;
+    }
+int main (){
+    vector <empleado> nomina;
+    vector <empleado> nomina_barata;
+    empleado prime;
+    cout<<"ingreso de empleados"<<endl;
+    ingreso(nomina);
+    prime=empleadoConMayorSueldo(nomina, nomina_barata);
+    cout<<"el empleado que mas cobra es: "<<prime.nombre<<" y cobra: "<<prime.salario<<" pesos al mes"<<endl;
+    cout<< "y los empleados que cobran menos de 400 k al mes son"<<endl;
+    for(empleado ju : nomina_barata){
+        cout<<ju.nombre<< " ";
+        cout<<ju.apellido<<endl;
+    }
+
+}
---
+++ e2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+/*Se quieren almacenar los productos de un supermercado. De estos interesa conocer el nombre, código de barra, marca, precio y si tiene fecha de vencimiento o no.
+
+Los pasillos del supermercado están compuestos por góndolas de 3 filas y 4 columnas. Ingresar tantos productos como sea necesario para llenar un pasillo y determinar:
+
+    Columna de la góndola que tiene el precio de mayor valor.
+
+    Nombre del producto de la fila 2, columna 3.
+
+    Promedio de precios de todo ese pasillo.
+
+    Cantidad de productos con fecha de vencimiento.
+
+*/
+struct producto{
+    string nombre;
+    int codigo_de_barra;
+    string marca;
+    int precio;
+    bool vencimiento;
+};
+
+void llenar_pasillo(producto pasillo[3][4]){
+    producto producto_generico1;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            cout<<"ingrese el producto que quiera en la fila:"<<i<<"y columna:"<<j<<endl;
+            cout<<"nombre del producto:"<<endl;
+            cin>>producto_generico1.nombre;
+            cout<<"codigo de barra:"<<endl;
+            cin>>producto_generico1.codigo_de_barra;
+            cout<<"marca:"<<endl;
+            cin>>producto_generico1.marca;
+            cout<<"precio:"<<endl;
+            cin>>producto_generico1.precio;
+            cout<<"vencimiento 0/1:"<<endl;
+            cin>>producto_generico1.vencimiento;
+            pasillo[i][j]= producto_generico1;
+        }
+    }
+}
+void precio_mayor_nombre_seleccionado(producto pasillo[3][4]){
+    int precio_m=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            if(pasillo[i][j].precio>precio_m){
+                precio_m=pasillo[i][j].precio;
+            }
+            if(i==2 && j==3){
+                cout<<"nombre del producto seleccionado: "<<pasillo[i][j].nombre<<endl;
+            }
+        }
+    }
+    cout<<precio_m;        
+}
+int promedio(producto pasillo[3][4]){
+    int promiedo=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            pasillo[i][j].precio+=promiedo;
+        }
+    }
+    promiedo/=12;
+    return promiedo;        
+}
+int cantidad_que_vencen(producto pasillo[3][4]){
+    int cantidad_que_v=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            if(pasillo[3][4].vencimiento==1){
+                cantidad_que_v+=1;
+            }
+        }
+    }
+    return cantidad_que_v;        
+}
+
+int main(){
+    producto pasillo[3][4];
+    llenar_pasillo(pasillo);
+    precio_mayor_nombre_seleccionado(pasillo);
+    cout<<promedio(pasillo)<<endl;
+    cout<<promedio(pasillo)<<endl;
+}
