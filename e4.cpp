856caff539924e76b2a0cba0a12a4073dabed424
SOrtiz1967 <166780651+SOrtiz1967@users.noreply.github.com>
2025-03-12 09:52:41 -0300

    Add files via upload

---
+++ e4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*Se quiere hacer un sistema de gestión de pacientes en una clínica. Cada paciente tiene los siguientes datos: nombre, edad, número de historia clínica, y fecha de próxima cita (día, mes y año como una estructura de tipo Fecha). El sistema debe permitir:
+Registrar nuevos pacientes y asignarles una fecha de cita.
+
+Cancelar una cita buscando al paciente por número de historia clínica.
+
+Mostrar todas las citas programadas para un día específico.
+
+Buscar y mostrar información de un paciente usando su número de historia clínica.*/
+struct fecha{
+    int dia;
+    int mes;
+    int anio;
+}
+struct paciente{
+    string nombre;
+    int edad;
+    int n_h_cl;
+    fecha fecha_p_c;
+
+}
+void registro_de_datos_citas(vector <paciente> &pacientes){
+    paciente paciente_generico;
+    condicion=true
+    while(condicion){
+        cout<<"Ingrese el nombre del paciente: "<<endl;
+        cout<<"Ingrese la edad del paciente: "<<endl;
+        cout<<"Ingrese el numero de historia clinica del paciente:"<<endl;
+        cout<<"Ingrese dia, mes y año de la fecha de proxima cita: "<<endl;
+    }
+}
+
+int main(){
+    vector <paciente> pacientes;
+}
