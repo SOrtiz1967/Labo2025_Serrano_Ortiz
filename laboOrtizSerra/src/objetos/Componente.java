afbd3691c752b334bcfd28f241d0276f4881466f
SOrtiz1967 <santiagogortiz@gmail.com>
2025-05-26 12:43:02 -0300

    finalizacion de el ejercicio 3 con correciones ost catedra de martin y avances en el 4

--- laboOrtizSerra/src/objetos/Componente.java
+++ laboOrtizSerra/src/objetos/Componente.java
@@ -1,11 +1,46 @@
+package objetos;
+
 public class Componente {
     private  String fabricante;
     private String modelo;
     private double precio;
     private int stock;
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
     public Componente(){
         this.fabricante="Agustin Urzi";
-        this.modelo="refrigeracion Caniche"
+        this.modelo="refrigeracion Caniche";
         this.precio=80000;
         this.stock=54;
     }
@@ -16,7 +51,7 @@ public class Componente {
         this.stock = stock;
     }
 
-    public void ajustarPrecioPorInflacion(double porcentaje) {
+    public void ajustarPorInflacion(double porcentaje) {
         this.precio = this.precio * (1 + porcentaje / 100);
     }
     public boolean hayStock(){
---
+++ laboOrtizSerra/src/objetos/Salida.java
@@ -0,0 +1,21 @@
+package objetos;
+
+import java.util.ArrayList;
+
+public class Salida extends Componente{
+    //pantallas e impresoras (puertos; para impresoras, método de impresión: inyección o láser)
+    private ArrayList<String> puertos;
+
+    public Salida(String fabricante, String modelo, double precio, int stock, ArrayList<String> puertos) {
+        super(fabricante, modelo, precio, stock);
+        this.puertos = puertos;
+    }
+
+    public ArrayList<String> getPuertos() {
+        return puertos;
+    }
+
+    public void setPuertos(ArrayList<String> puertos) {
+        this.puertos = puertos;
+    }
+}
