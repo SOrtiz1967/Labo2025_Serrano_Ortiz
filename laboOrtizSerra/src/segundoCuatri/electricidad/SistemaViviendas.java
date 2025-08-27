ac097be72fe741ff9bed4cda78e744f1452813ff
santi <santiagogortiz@gmail.com>
2025-08-27 00:12:54 -0300

    ejercicio 1 de repaso terminado a falta de el main

--- laboOrtizSerra/src/segundoCuatri/electricidad/SistemaViviendas.java
+++ laboOrtizSerra/src/segundoCuatri/electricidad/SistemaViviendas.java
@@ -1,10 +1,32 @@
 package segundoCuatri.electricidad;
 
-import java.time.Month;
-import java.util.HashMap;
+import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class SistemaViviendas {
-    private HashMap<Vivienda, HashMap<Integer,HashMap<Month, Consumo>>> viviendas;
+    private HashSet<Vivienda> viviendas;
 
+    public SistemaViviendas(HashSet<Vivienda> viviendas) {
+        this.viviendas = viviendas;
+    }
+
+    public HashSet<Vivienda> getViviendas() {
+        return viviendas;
+    }
+
+    public void setViviendas(HashSet<Vivienda> viviendas) {
+        this.viviendas = viviendas;
+    }
+    public void cargarConsumosMes() {
+        Scanner sc = new Scanner(System.in);
+        LocalDate hoy = LocalDate.now();
+
+        for (Vivienda v : viviendas) {
+            System.out.println("Ingrese consumo en KWh para la vivienda en: " + v.getDireccion());
+            int consumo = sc.nextInt();
+
+            v.cargarConsumo(hoy, consumo);
+        }
+    }
 }
