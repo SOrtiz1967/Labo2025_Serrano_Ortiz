97e4b1f772e2a1ca06327d15acdf968300690f62
santi <santiagogortiz@gmail.com>
2025-08-27 00:26:36 -0300

    ejercicio 1 de repaso terminado a falta de el main

---
+++ laboOrtizSerra/src/segundoCuatri/electricidad/Main.java
@@ -0,0 +1,12 @@
+package segundoCuatri.electricidad;
+
+import java.time.Month;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
